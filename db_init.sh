202b3aba6e673cca5380e727ce726d5a613afce4
root <luke.c.sargent@gmail.com>
2016-04-20 09:10:20 -0400

    arp scan results to bash variable

--- db_init.sh
+++ db_init.sh
@@ -3,11 +3,20 @@ clear
 
 # function for quitting in error cases
 function quit {
-    printf "\n\t(╯°□°)╯︵ ┻━┻                 \n"
+    printf "\n\t  (╯°□°)╯︵ ┻━┻                 \n"
     printf "  ...hope this wasn't during the presentation...\n\n"
     exit
 }
 
+#   is_zero string msg
+# if argument $1 is empty, print arg $2 as an error message
+function is_empty {
+    if [[ -z $1 ]]
+    then
+        printf "ERROR: $2 \n"
+        quit
+    fi
+}
 
 BANNER="
  █████╗ ██╗   ██╗████████╗ ██████╗ ███████╗ ██████╗ █████╗ ███╗   ██╗
@@ -23,11 +32,7 @@ printf "$BANNER"
 
 # detect active network interface
 NIC=$(ip link show | grep 'state UP' | awk -F ': |:' '{print $2}')
-if [[ "$NIC" == "" ]]
-then
-    echo "No active connections"
-    quit
-fi
+is_empty $NIC "no available network connection"
 
 #acquire and print out connection information
 printf "Network Connection:\n"
@@ -43,6 +48,23 @@ ifconfig $NIC up
 #reconnect to network with new mac -- might be a better way this takes some time
 nmcli con up id "$ESSID"
 
+# arp scan parsing: returns a list of space-separated IPs
+ARPRESULT=$(arp-scan --localnet | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' | tr '\n' ' ')
+# error check
+is_empty $ARPRESULT "ARP scan failure"
+#remove trailing space 
+ARPRESULT=${ARPRESULT:0:-1}
+
+# turn string into array for individual host scanning
+HOSTS=($ARPRESULT)
+
+# nmap output file
+OUTFILE="arp-scan.378"
+
+
+# sample nmap scan using arp-scan result 
+#nmap -Pn -p5357 ${HOSTS[1]} > $OUTFILE
+
 # checking on status of postgresql service
 printf "checking database...\n"
 CHECK=$(service postgresql status | grep dead)
@@ -51,11 +73,10 @@ if [[ "$CHECK" == "   Active: inactive (dead)" ]]; then
     service postgresql start
 fi
 
-
-
 # database initialization 
 msfdb init
 msfdb start
+
 
 #launch metasploit with script file -- not yet implemented
 #msfconsole -r msfc_footprint.rc
